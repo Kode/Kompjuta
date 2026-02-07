cc3ae87f451b193ea1dda075f658de633f9aa0ea
Robert Konrad <git2016@robdangero.us>
2026-02-07 17:20:32 +0100

    Implement vle16.v and fix vs<eew>_vs<nf>r

--- sources/kompjuta.c
+++ sources/kompjuta.c
@@ -631,6 +631,13 @@ static void opcode_flw_vle(cpu_core *core, uint32_t instruction) {
 		break;
 	}
 	case 0x5: { // vle16.v
+		uint8_t vd = (instruction >> 7) & 0x1f;
+
+		uint64_t base = core->x[rs1];
+
+		for (uint16_t i = 0; i < core->vl; ++i) {
+			core->v[vd].values.u16[i] = *(uint16_t *)(&ram[base + 2 * i]);
+		}
 		break;
 	}
 	default:
@@ -667,30 +674,24 @@ static void opcode_fsw(cpu_core *core, uint32_t instruction) {
 			uint8_t width = (instruction >> 12) & 0x7;
 			switch (width) {
 			case 0x0: { // 8 bit
-				assert(core->sew == 8);
-
 				uint64_t base = core->x[rs1];
 
 				for (uint16_t i = 0; i < core->vl; ++i) {
-					*(uint8_t *)(&ram[base + 4 * i]) = core->v[vs3].values.u8[i];
+					*(uint8_t *)(&ram[base + 1 * i]) = core->v[vs3].values.u8[i];
 				}
 
 				break;
 			}
 			case 0x5: { // 16 bit
-				assert(core->sew == 16);
-
 				uint64_t base = core->x[rs1];
 
 				for (uint16_t i = 0; i < core->vl; ++i) {
-					*(uint16_t *)(&ram[base + 4 * i]) = core->v[vs3].values.u16[i];
+					*(uint16_t *)(&ram[base + 2 * i]) = core->v[vs3].values.u16[i];
 				}
 
 				break;
 			}
 			case 0x6: { // 32 bit
-				assert(core->sew == 32);
-
 				uint64_t base = core->x[rs1];
 
 				for (uint16_t i = 0; i < core->vl; ++i) {
@@ -700,8 +701,6 @@ static void opcode_fsw(cpu_core *core, uint32_t instruction) {
 				break;
 			}
 			case 0x7: // 64 bit
-				assert(core->sew == 64);
-
 				uint64_t base = core->x[rs1];
 
 				for (uint16_t i = 0; i < core->vl; ++i) {
